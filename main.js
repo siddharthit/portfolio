e5be8c36676048faa57c651984bcf2f4b6623ca0
Siddharth Verma <139902541+siddharthit@users.noreply.github.com>
2023-10-02 01:00:16 +0530

    added

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".auto-type", {
+    strings: [" Full stack Developer ","Front-End Developer ","Back-End Developer","Digital Marketing Expert"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
